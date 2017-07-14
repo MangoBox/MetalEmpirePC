ad60c9e774acb05ac47bb6e82a5116c691441e6e
MangoBox <mangobox228@hotmail.com.au>
2017-07-14 20:02:21 +1000

    Fixed some directory structures and rewrote some references.

--- MetalEmpirePC/reviewPage.php
+++ MetalEmpirePC/reviewPage.php
@@ -3,54 +3,61 @@
 <title>Write Review</title>
 <body>
 	<br>
-	<h1 title="test">MetalEmpirePC Register</h1>
+	
 	<br>
 	<form method = "post", action="/MetalEmpirePC/user_review.php">
-		<fieldset>
-			<legend>Write Review</legend>
+	<div style="width: 30%; margin: 0 auto;">
+		<h1 title="test">Write a Review</h1>
+			<fieldset>
+				<legend>Write Review</legend>
 
-			Review Title: <input type="text" name="title" color="gray">
-			<br>
-			<br>
-			Rating:
-			<br>
-			<input type="radio" name="rating" value="rating1"> ★☆☆☆☆<br>
-			<input type="radio" name="rating" value="rating2"> ★★☆☆☆<br>
-			<input type="radio" name="rating" value="rating3"> ★★★☆☆<br>
-			<input type="radio" name="rating" value="rating4"> ★★★★☆<br>
-			<input type="radio" name="rating" value="rating5"> ★★★★★<br>
-  			
-  			<br>
-  			<select name = "product_select">
-	  			<option selected="selected">Select a Product</option>
-				<option>Core A1</option>
-				<option>Core A2</option>
-				<option>Core A3</option>
-				<option>Magma B1</option>
-				<option>Magma B2</option>
-				<option>Magma B3</option>
-				<option>Titanium X1</option>
-				<option>Titanium X2</option>
-				<option>Titanium X3</option>
-			</select>
+				Review Title: <input type="text" name="title" color="gray">
+				<br>
+				<br>
+				Rating:
+				<br>
+				<input type="radio" name="rating" value="rating1"> ★☆☆☆☆<br>
+				<input type="radio" name="rating" value="rating2"> ★★☆☆☆<br>
+				<input type="radio" name="rating" value="rating3"> ★★★☆☆<br>
+				<input type="radio" name="rating" value="rating4"> ★★★★☆<br>
+				<input type="radio" name="rating" value="rating5"> ★★★★★<br>
 
-			<br>
-			<br>
-			Review Content: <br><br>
-			<textarea rows="6" cols="50" name = "review_text"></textarea>
-			<br>
-			<br>
-			<?php
+				<br>
+				<select name = "product_select">
+					<option selected="selected">Select a Product</option>
+					<option>Core A1</option>
+					<option>Core A2</option>
+					<option>Core A3</option>
+					<option>Magma B1</option>
+					<option>Magma B2</option>
+					<option>Magma B3</option>
+					<option>Titanium X1</option>
+					<option>Titanium X2</option>
+					<option>Titanium X3</option>
+				</select>
+
+				<br>
+				<br>
+				<div style="width: 100%; margin: 0 auto;">
+					Review Content: <br><br>
+					<textarea rows="6" cols="50" name = "review_text"></textarea>
+				</div>
+				<br>
+				<br>
+				<?php
 				session_start();
 				if(isset($_SESSION["username"])) {
 					echo "<input type=\"submit\" value=\"Post Review\">";
 				} else {
-					echo "<input type=\"submit\" value=\"Post Review\" disabled>";
+					echo "<input type=\"submit\" value=\"Post Review\" disabled><br>";
+					echo "You must be logged in to post reviews!<br>";
+					echo "<a href = \"loginPage.php\">Log In</a>";
 				}
 
-			?>
-			
-		</fieldset>
-	</form>
+				?>
+
+			</fieldset>
+		</form>
+	</div>
 </body>
 </html>
