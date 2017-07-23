9eb0733cb35cc9d4aaf50ebc2003c4675dd023be
MangoBox <mangobox228@hotmail.com.au>
2017-07-23 17:31:49 +1000

    Some additional fixes.

--- MetalEmpirePC/logout.php
+++ MetalEmpirePC/logout.php
@@ -1,12 +1,6 @@
 <?php
 	session_start();
 	session_destroy();	
+
+	header("location: loginPage.php");
  ?>
-
-
- <html>
- 	<body>
- 		You have been logged out.
- 		<a href = "loginPage.php"> Back to login page </a>
- 	</body>
- </html>
